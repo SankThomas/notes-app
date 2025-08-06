425a6da2491cffbc66430965077f22b16a44ae02
SankThomas <thomassankara74@gmail.com>
2025-08-06 14:46:10 +0300

    remove unused functions

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -12,20 +12,3 @@ export function formatDate(date) {
     year: "numeric",
   }).format(new Date(date));
 }
-
-export function generateId() {
-  return Math.random().toString(36).substr(2, 9);
-}
-
-export function extractTags(content) {
-  const tagRegex = /#[\w-]+/g;
-  const matches = content.match(tagRegex);
-  return matches ? matches.map((tag) => tag.substring(1)) : [];
-}
-
-export function highlightSearchTerm(text, searchTerm) {
-  if (!searchTerm) return text;
-
-  const regex = new RegExp(`(${searchTerm})`, "gi");
-  return text.replace(regex, '<mark class="bg-yellow-200">$1</mark>');
-}
